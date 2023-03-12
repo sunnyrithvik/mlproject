14e683e3a1b4daddfcd8a02188a47a236aeda6e9
sunnyrithvik <sribhashyamrithvik@gmail.com>
2023-03-12 10:26:50 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-#from src.components.data_transformation import DataTransformation
-#from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -40,6 +40,7 @@ class DataIngestion:
             logging.info("Ingestion of the data is completed")
 
             return(
+                
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
 
@@ -50,3 +51,8 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    
